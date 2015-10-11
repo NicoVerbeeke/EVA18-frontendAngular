85b27ad3f6fd6441725fb7c73cdba18ed6d200bb
NicoVerbeeke <nico.verbeeke.t2727@student.hogent.be>
2015-10-11 16:39:40 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name eva18FrontendAngularApp
+ * @description
+ * # eva18FrontendAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('eva18FrontendAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eva18FrontendAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eva18FrontendAngularApp
+ */
+angular.module('eva18FrontendAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
